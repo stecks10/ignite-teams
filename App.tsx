a29b93dc1a652836e1294f19356a689d24babcef
Vitor Nunes do Nascimento <stecks10@gmail.com>
2024-11-18 18:42:05 -0300

    Definindo o tema da aplicação

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
+import { ThemeProvider } from "styled-components/native";
 import { Groups } from "./src/screens/Groups";
+import theme from "./src/theme";
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <StatusBar style="light" backgroundColor="transparent" translucent />
       <Groups />
-    </>
+    </ThemeProvider>
   );
 }
