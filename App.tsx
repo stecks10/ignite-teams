0c4f8f753f998ed324c59902de31b33e3622c76b
Vitor Nunes do Nascimento <stecks10@gmail.com>
2024-11-18 19:30:14 -0300

    Fontes personalizadas

--- App.tsx
+++ App.tsx
@@ -1,15 +1,26 @@
-import { StatusBar } from "expo-status-bar";
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+import { ActivityIndicator } from "react-native";
 import { ThemeProvider } from "styled-components/native";
 import { Groups } from "./src/screens/Groups";
 import theme from "./src/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <ThemeProvider theme={theme}>
-      <>
-        <StatusBar style="light" backgroundColor="transparent" translucent />
-        <Groups />
-      </>
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
