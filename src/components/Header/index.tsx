cd3c311e131ac21a4c306978cccd72c54035fd67
Vitor Nunes do Nascimento <stecks10@gmail.com>
2024-11-19 16:55:10 -0300

    Biblioteca de ícones

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,19 @@
 import logoImg from "../../assets/logo.png";
-import { Container, Logo } from "./styles";
-export function Header() {
+import { BackButton, BackIcon, Container, Logo } from "./styles";
+
+type Props = {
+  showBackButton?: boolean;
+};
+
+export function Header({ showBackButton = false }: Props) {
   return (
     <Container>
+      {showBackButton && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
+
       <Logo source={logoImg} />
     </Container>
   );
