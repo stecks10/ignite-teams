a5c6ac47cd1906dedb032a77111f4ef4985eca0f
Vitor Nunes do Nascimento <stecks10@gmail.com>
2024-11-21 16:13:25 -0300

    Listando grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,22 @@
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { useState } from "react";
+import { FlatList } from "react-native";
 import { Container } from "./styles";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Galara da turma"]);
+
   return (
     <Container>
       <Header showBackButton />
       <Highlight title="Turmas " subtitle="Jogue com sua turma" />
-      <GroupCard title="Nome da Turma" />
-      <GroupCard title="Galera" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
