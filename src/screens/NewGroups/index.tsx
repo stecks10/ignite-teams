f835e1475d87b78176e8dded51bebce2943051fa
Vitor Nunes do Nascimento <stecks10@gmail.com>
2024-11-21 17:31:22 -0300

    Criando Componente Input

--- src/screens/NewGroups/index.tsx
+++ src/screens/NewGroups/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { Input } from "@components/Input";
 import { Container, Content, Icon } from "./styles";
 
 export function NewGroups() {
@@ -15,7 +16,9 @@ export function NewGroups() {
           subtitle="Crie a turma para adicionar as pessoas"
         />
 
-        <Button title="Criar" />
+        <Input />
+
+        <Button title="Criar" style={{ marginTop: 20 }} />
       </Content>
     </Container>
   );
