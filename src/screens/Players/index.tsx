faa612bae1e68524ff33f802b0f061fe13f253e0
Vitor Nunes do Nascimento <stecks10@gmail.com>
2024-11-22 08:42:22 -0300

    Componente Filter

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@components/Button";
 import { ButtonIcon } from "@components/ButtonIcon";
+import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
@@ -17,6 +19,13 @@ export function Players() {
         <Input placeholder="Nome do Participante" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
+      <Filter title="TIME A" isActive style={{ marginTop: 12 }} />
+
+      <Button
+        style={{ marginTop: 400 }}
+        title="Remover turma"
+        type="SECONDARY"
+      />
     </Container>
   );
 }
