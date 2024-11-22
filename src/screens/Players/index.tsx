faf73c6e83cb31cb0e4ed6c72182f9338b9787db
Vitor Nunes do Nascimento <stecks10@gmail.com>
2024-11-22 09:20:07 -0300

    Criando o Componente PlayerCard

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,27 +1,31 @@
-import { Button } from "@components/Button";
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
-import { useState } from "react";
-import { FlatList } from "react-native";
-import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
+import { PlayerCard } from "@components/PlayerCard";
+
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 export function Players() {
   const [team, setTeam] = useState("Time A");
-  const [players, setPlayers] = useState(["1"]);
+  const [players, setPlayers] = useState(["Maria", "Vini"]);
 
   return (
     <Container>
       <Header showBackButton />
+
       <Highlight
         title="Nome da turma"
-        subtitle="Adicione a galera e se divirta"
+        subtitle="adicione a galera e separe os times"
       />
 
       <Form>
-        <Input placeholder="Nome do Participante" autoCorrect={false} />
+        <Input placeholder="Nome da pessoa" autoCorrect={false} />
+
         <ButtonIcon icon="add" />
       </Form>
 
@@ -38,13 +42,16 @@ export function Players() {
           )}
           horizontal
         />
-        <NumbersOfPlayers>{players}</NumbersOfPlayers>
+
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
       </HeaderList>
 
-      <Button
-        style={{ marginTop: 400 }}
-        title="Remover turma"
-        type="SECONDARY"
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
       />
     </Container>
   );
